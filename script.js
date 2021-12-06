6edcb6978958195bb25ed856f071527cffb4f2d9
madelineemerson <60785028+madelineemerson@users.noreply.github.com>
2021-12-05 23:32:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*
+
 This is your site JavaScript code - you can add interactivity and carry out processing
 - Initially the JS writes a message to the console, and moves a button you can add from the README
 */
@@ -7,7 +7,7 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Violent Crime in Kansas");
 
-/* 
+* 
 Make the "Click me!" button move when the visitor clicks it:
 - First add the button to the page by following the "Next steps" in the README
 */
